239e0d7544983f08169a05388c21f85a18450fe2
Helena Rosenzweig <helena.rosenzw@gmail.com>
2022-09-27 18:23:11 +0200

    add endpoint for serving static files for heroku deployment remove comments

--- src/model/db.ts
+++ src/model/db.ts
@@ -3,25 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-import Todo from './schema/todo.schema'
+const MONGODB_URI = process.env["MONGODB_URI"] ?? ""
 
-const USER = process.env["DB_USER"];
-const PASSWORD = process.env["DB_PASSWORD"];
-const DB_URL = `mongodb+srv://${USER}:${PASSWORD}@cluster0.hkogdrz.mongodb.net/?retryWrites=true&w=majority\n`;
-
-mongoose.connect(DB_URL).then(()=>{
+mongoose.connect(MONGODB_URI).then(()=>{
     console.log("successful connection")
 }).catch((err)=>{
     console.log("error connecting to db")
+    console.log(err)
 });
-/*
-async function createTodo(){
-    const todo1 = new Todo({description: 'Water plants'})
-    await todo1.save()
-    const todo2 = new Todo({description: 'Pay bills'})
-    await todo2.save()
-    const todo3 = new Todo({description: 'Feed the dog'})
-    await todo3.save()
-}
-createTodo()
-*/
+
--- src/server/index.ts
+++ src/server/index.ts
@@ -6,10 +6,12 @@ import cors from 'cors';
 import bodyParser from "body-parser";
 import * as Sentry from "@sentry/node";
 import * as Tracing from "@sentry/tracing";
+import path from "path";
 
 dotenv.config();
 
 const app = express()
+app.use(express.static('public'))
 app.use(bodyParser.json());
 
 app.use(cors({
@@ -17,7 +19,6 @@ app.use(cors({
 }));
 
 app.use('/', router);
-
 
 Sentry.init({
     dsn: "https://3afa1f22256e48bda1e4e3b4c721c6cf@o1425867.ingest.sentry.io/6774464",
@@ -34,7 +35,7 @@ Sentry.init({
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 1.0
     // or pull from params
     // tracesSampleRate: parseFloat(params.SENTRY_TRACES_SAMPLE_RATE),
 });
@@ -61,12 +62,11 @@ app.use(function onError(err:any, req:any, res:any, next:any) {
     res.end(res.sentry + "\n");
 });
 
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+})
 
-
-
-
-
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log("Server started on port " + PORT);
